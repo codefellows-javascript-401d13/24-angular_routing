0717b6a5654586c8dabb9513a03ff00ffffb8d7c
radenska <yana_radenska@yahoo.com>
2017-03-27 12:15:37 -0700

    finished router-config file

--- lab-yana/app/entry.js
+++ lab-yana/app/entry.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// const HTMLPlugin = require('html-webpack-plugin');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const camelcase = require('camelcase');
 const pascalcase = require('pascalcase');
@@ -18,18 +16,3 @@ context.keys().forEach(key => {
   let name = pascalcase(path.basename(key, '.js'));
   routesApp.controller(name, context(key));
 });
-
-// module.exports = {
-//   entry: `${__dirname}/app/entry.js`,
-//   output: {
-//     filename: 'bundle.js',
-//     path: `${__dirname}/build`
-//   },
-//   plugins: [
-//     new HTMLPlugin({ template: `${__dirname}/app/index.html`} ),
-//     new ExtractTextPlugin('bundle.css')
-//   ],
-//   rules: {
-//
-//   }
-// };
