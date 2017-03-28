1c530654e95801f44da212d9e19f79bdd21cfd76
Remil Marzan <remilonwheels@gmail.com>
2017-03-27 20:39:13 -0700

    updated styles

--- lab-remil/app/view/home-view/home-view-controller.js
+++ lab-remil/app/view/home-view/home-view-controller.js
@@ -2,9 +2,10 @@
 
 require('./home-view.scss');
 
-
 function HomeViewController($log) {
   $log.debug('HomeViewController');
+
+  this.title = 'Home sweet home';
 }
 
 module.exports = ['$log', HomeViewController];
--- lab-remil/app/view/signup-view/signup-view-controller.js
+++ lab-remil/app/view/signup-view/signup-view-controller.js
@@ -5,6 +5,8 @@ require('./signup-view.scss');
 
 function SignupViewController($log) {
   $log.debug('SignupViewController');
+
+  this.title = 'Sign up for da swag';
 }
 
 module.exports = ['$log', SignupViewController];
